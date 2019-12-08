bd6e7126932992d590b96a8a8e3ea7b003c03622
superwhiskers <whiskerdev@protonmail.com>
2019-12-08 06:01:27 +0000

    2019: five.nim: finally, the perfect intcode interpreter

--- 2019/five.nim
+++ 2019/five.nim
@@ -6,75 +6,115 @@ import os
 import sequtils
 import strformat
 
-proc parseArgs(modes: seq[int]; args: seq[int]; insts: ref seq[int]): seq[int] =
+# processes modes and arguments and pairs them up
+proc argModePair(modes: seq[int]; args: seq[string]): seq[tuple[arg: int, mode: int]] =
   result = @[]
-  for i, m in modes:
-    if m == 0: # pointer
-      result.insert(0, insts[args[i+(args.len-i)]])
-    elif m == 1: # literal
-      result.insert(0, args[i+(args.len-i)])
+  for i in 0..args.len-1:
+    if i < modes.len:
+      result.add((arg: parseInt(args[i]), mode: modes[modes.len - i - 1]))
     else:
-      echo("invalid mode: {m}")
+      result.add((arg: parseInt(args[i]), mode: 0))
 
+# converts a mode,arg tuple into an argument
+proc convertModePairToArg(pair: tuple[arg: int, mode: int], insts: ref seq[string]): int =
+  case pair.mode
+  of 0:    # pointer
+    var pos = pair.arg
+    while pos < 0:
+      pos = insts[].len + pos
+    result = parseInt(insts[pos])
+  of 1:    # literal
+    result = pair.arg
+  else:
+    echo(fmt"unknown mode: {pair.mode}")
 
-when not declared(commandLineParams):
+# handle missing intcode
+if not declared(commandLineParams) or commandLineParams().len == 0:
   echo("no intcode was passed to the program. exiting")
+  quit(1)
 
 # nim's pointer types are disgusting
 var insts: ref seq[string]
 new(insts)
-insts = commandLineParams()[0].split(',')
+insts[] = commandLineParams()[0].split(',')
 
-
+# main execution loop
 block ExecutionLoop:
-  for i, g in insts:
+  var i = 0
+  while true:
     # parse the instruction modes and the instruction itself
-    let inst = parseInt(g[^2..g.len])
-    let modes = g[0..^2].split("").map(proc (i: string): int = parseInt(i))
-    var args: seq[int]
+    var inst = parseInt(insts[i])
+    var modes: seq[int]
+    if insts[i].len > 2:
+      inst = parseInt(insts[i][^2..insts[i].len-1])
+      modes = toSeq(insts[i][0..^3].items).map(proc (i: char): int = parseInt($i))
+    var modepair: seq[tuple[arg: int, mode: int]]
 
     # execute the instructions
     case inst
     of 1:  # c = add(a, b)
       # parse arguments
-      args = parseArgs(modes, insts[i+1..i+3], new())
-
-      # fail if the third argument is in literal mode
-      if modes[0] == 1:
-        echo("add: third argument cannot be in literal mode")
+      modepair = argModePair(modes, insts[i+1..i+3])
 
       # perform the operation
-      insts[args[2]] = $(args[0] + args[1])
+      insts[modepair[2].arg] = $(convertModePairToArg(modepair[0], insts) + convertModePairToArg(modepair[1], insts))
     of 2:  # c = multiply(a, b)
       # parse arguments
-      args = parseArgs(modes, insts[i+1..i+3], ref insts)
-
-      # fail if the third argument is in literal mode
-      if modes[0] == 1:
-        echo("multiply: third argument cannot be in literal mode")
+      modepair = argModePair(modes, insts[i+1..i+3])
 
       # perform the operation
-      insts[args[2]] = $(args[0] * args[1])
+      insts[modepair[2].arg] = $(convertModePairToArg(modepair[0], insts) * convertModePairToArg(modepair[1], insts))
     of 3:  # a = input()
       # parse arguments
-      args = parseArgs(modes, @[insts[i+1]], ref insts)
-
-      # fail if the only argument is in literal mode
-      if modes[0] == 1:
-        echo("input: first argument cannot be in literal mode")
+      modepair = argModePair(modes, @[insts[i+1]])
 
       # perform the operation
-      insts[args[0]] = readline(stdin)
+      stdout.write("input: ")
+      insts[modepair[0].arg] = readline(stdin)
     of 4:  # output(a)
       # parse arguments
-      args = parseArgs(modes, @[insts[i+1]], ref insts)
-
-      # fail if the only argument is in literal mode
-      if modes[0] == 1:
-        echo("output: first argument cannot be in literal mode")
+      modepair = argModePair(modes, @[insts[i+1]])
 
       # perform the operation
-      echo(insts[args[0]])
+      echo(fmt"output: {insts[modepair[0].arg]}")
+    of 5: # jump_if_true(a, b)
+      # parse arguments
+      modepair = argModePair(modes, insts[i+1..i+2])
+
+      # perform the operation
+      if convertModePairToArg(modepair[0], insts) != 0:
+        i = convertModePairToArg(modepair[1], insts)
+
+        # skip automatic ip incrementation
+        continue
+    of 6: # jump_if_false(a, b)
+      # parse arguments
+      modepair = argModePair(modes, insts[i+1..i+2])
+
+      # perform the operation
+      if convertModePairToArg(modepair[0], insts) == 0:
+        i = convertModePairToArg(modepair[1], insts)
+
+        # skip automatic ip incrementation
+        continue
+    of 7: # c = lt(a, b)
+      # parse arguments
+      modepair = argModePair(modes, insts[i+1..i+3])
+
+      # perform the operation
+      if convertModePairToArg(modepair[0], insts) < convertModePairToArg(modepair[1], insts):
+        insts[modepair[2].arg] = "1"
+      else:
+        insts[modepair[2].arg] = "0"
+    of 8: # c = eq(a, b)
+      # parse arguments
+      modepair = argModePair(modes, insts[i+1.. i+3])
+
+      # perform the operation
+      if convertModePairToArg(modepair[0], insts) == convertModePairToArg(modepair[1], insts):
+        insts[modepair[2].arg] = "1"
+      else:
+        insts[modepair[2].arg] = "0"
     of 99: # exit(0)
       break ExecutionLoop
     else:
@@ -82,8 +122,8 @@ block ExecutionLoop:
       break ExecutionLoop
 
     # increment instruction pointer
-    i += args.len + 1
+    i += modepair.len + 1
 
 # output the result
-echo(&"output=[{insts}]")
+echo(fmt"memory={insts[]}")
 
